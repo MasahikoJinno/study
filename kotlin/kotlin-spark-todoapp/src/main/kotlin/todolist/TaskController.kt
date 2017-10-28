7d9de996a361b189b77041be0a9be1d819c42499
Masahiko Jinno <miracle_t_shirt09@yahoo.co.jp>
2017-10-28 21:00:23 +0900

    CRUD

--- kotlin/kotlin-spark-todoapp/src/main/kotlin/todolist/TaskController.kt
+++ kotlin/kotlin-spark-todoapp/src/main/kotlin/todolist/TaskController.kt
@@ -1,14 +1,51 @@
 package todolist
 
-import spark.Request
-import spark.Response
+import com.fasterxml.jackson.databind.ObjectMapper
 import spark.Route
+import spark.Request
+import spark.Spark.halt
 
-class TaskController {
+class TaskController(
+        private val objectMapper: ObjectMapper,
+        private val taskRepository: TaskRepository
+) {
+    private val Request.task: Task?
+        get() {
+            val id = params("id").toLongOrNull()
+            return id?.let(taskRepository::findById)
+        }
+
     fun index(): Route = Route { req, res ->
-        listOf(
-                Task(1, "クリーニングに出す", false),
-                Task(2, "テストーーー", true)
+        taskRepository.findAll()
+    }
+
+    fun create(): Route = Route { req, res ->
+        val request: TaskCreateRequest =
+                objectMapper.readValue(req.bodyAsBytes()) ?: throw halt(400)
+        val task = taskRepository.create(request.content)
+        res.status(201)
+        task
+    }
+
+    fun show(): Route = Route { req, res ->
+        req.task ?: throw halt(404)
+    }
+
+    fun destroy(): Route = Route { req, res ->
+        val task = req.task ?: throw halt(404)
+        taskRepository.delete(task)
+        res.status(204)
+    }
+
+    fun update(): Route = Route { req, res ->
+        val request: TaskUpdateRequest =
+                objectMapper.readValue(req.bodyAsBytes()) ?: throw halt(400)
+        val task = req.task ?: throw halt(404)
+        val newTask = task.copy(
+                content = request.content ?: task.content,
+                done = request.done ?: task.done
         )
+        taskRepository.update(newTask)
+        res.status(204)
     }
 }
--- kotlin/kotlin-spark-todoapp/src/main/kotlin/todolist/main.kt
+++ kotlin/kotlin-spark-todoapp/src/main/kotlin/todolist/main.kt
@@ -2,12 +2,26 @@ package todolist
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import spark.Spark.path
 import spark.Spark.get
+import spark.Spark.post
+import spark.Spark.delete
+import spark.Spark.patch
 
 fun main(args: Array<String>) {
     val objectMapper = ObjectMapper().registerKotlinModule()
     val jsonTransformer = JsonTransformer(objectMapper)
-    val taskController = TaskController()
+    val taskRepository = TaskRepository()
+    val taskController = TaskController(objectMapper, taskRepository)
 
-    get("/tasks", taskController.index(), jsonTransformer)
+    path("/tasks") {
+        get("", taskController.index(), jsonTransformer)
+    }
+
+    path("/task") {
+        get("/:id", taskController.show(), jsonTransformer)
+        post("/create", taskController.create(), jsonTransformer)
+        delete("/:id", taskController.destroy(), jsonTransformer)
+        patch("/:id", taskController.update(), jsonTransformer)
+    }
 }
