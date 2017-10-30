64cd7dbddf7f1479e859c21ec1beb9a378612da6
Masahiko Jinno <miracle_t_shirt09@yahoo.co.jp>
2017-10-31 07:29:35 +0900

    not need import delete

--- kotlin/kotlin_springboot_todoapp/src/main/kotlin/com/example/todolist/TaskController.kt
+++ kotlin/kotlin_springboot_todoapp/src/main/kotlin/com/example/todolist/TaskController.kt
@@ -1,6 +1,5 @@
 package com.example.todolist
 
-import javassist.NotFoundException
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.servlet.mvc.support.RedirectAttributes
 import org.springframework.validation.BindingResult
 import org.springframework.validation.annotation.Validated
 
