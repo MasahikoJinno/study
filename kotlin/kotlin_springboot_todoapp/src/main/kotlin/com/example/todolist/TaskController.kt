1280f218aa001c7464e69915a4661effaca95cca
Masahiko Jinno <miracle_t_shirt09@yahoo.co.jp>
2017-10-30 22:05:51 +0900

    add create task function

--- kotlin/kotlin_springboot_todoapp/src/main/kotlin/com/example/todolist/TaskController.kt
+++ kotlin/kotlin_springboot_todoapp/src/main/kotlin/com/example/todolist/TaskController.kt
@@ -4,14 +4,33 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
 
 @Controller
 @RequestMapping("tasks")
 class TaskController(private val taskRepository: TaskRepository) {
-    @GetMapping
+    @GetMapping("")
     fun index(model: Model): String {
         val tasks = taskRepository.findAll()
         model.addAttribute("tasks", tasks)
         return "tasks/index"
+    }
+
+    @GetMapping("new")
+    fun new(form: TaskCreateForm): String {
+        return "tasks/new"
+    }
+
+    @PostMapping("")
+    fun create(@Validated form: TaskCreateForm, bindingResult: BindingResult): String {
+        if (bindingResult.hasErrors()) {
+            return "tasks/new"
+        }
+        val content = requireNotNull(form.content)
+        taskRepository.create(content)
+        return "redirect:/tasks"
     }
 }
