29ed31421cc7c6c825e7bdc5bfd1ecf854428751
Masahiko Jinno <miracle_t_shirt09@yahoo.co.jp>
2017-10-31 07:43:24 +0900

    add error handling

--- kotlin/kotlin_springboot_todoapp/src/main/kotlin/com/example/todolist/TaskController.kt
+++ kotlin/kotlin_springboot_todoapp/src/main/kotlin/com/example/todolist/TaskController.kt
@@ -1,7 +1,10 @@
 package com.example.todolist
 
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -53,4 +56,8 @@ class TaskController(private val taskRepository: TaskRepository) {
         taskRepository.update(newTask)
         return "redirect:/tasks"
     }
+
+    @ExceptionHandler(NotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleNotFoundException(): String = "tasks/not_found"
 }
