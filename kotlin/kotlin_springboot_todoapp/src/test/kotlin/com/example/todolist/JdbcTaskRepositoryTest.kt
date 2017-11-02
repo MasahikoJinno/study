4c25e8198a12d6bc4874f1387467c2e3fd206574
Masahiko Jinno <miracle_t_shirt09@yahoo.co.jp>
2017-11-02 21:51:39 +0900

    add test case

--- kotlin/kotlin_springboot_todoapp/src/test/kotlin/com/example/todolist/JdbcTaskRepositoryTest.kt
+++ kotlin/kotlin_springboot_todoapp/src/test/kotlin/com/example/todolist/JdbcTaskRepositoryTest.kt
@@ -5,10 +5,12 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
+@RunWith(value = SpringRunner::class)
 @SpringBootTest
+@Sql(statements = arrayOf("DELETE FROM task"))
 class JdbcTaskRepositoryTest {
 
     @Autowired
@@ -18,5 +20,12 @@ class JdbcTaskRepositoryTest {
     fun 何もしなければfindAllはからのリストを返す() {
         val got = sut.findAll()
         assertThat(got).isEmpty()
+    }
+
+    @Test
+    fun createで作成したタスクをfindByIdで取得できる() {
+        val task = sut.create("TEST")
+        val got = sut.findById(task.id)
+        assertThat(got).isEqualTo(task)
     }
 }
